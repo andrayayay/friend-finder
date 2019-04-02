b2a77cf09782f616a60cebc403c31fb612638a54
Andrayayay <andrayayay@gmail.com>
2019-04-01 22:03:23 -0500

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var friends = require('./app/data/friends');
+var friends = require('./app/data/friends.js');
 var app = express();
 var PORT = process.env.PORT || 4000;
 
